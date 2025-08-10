592d2b859ead169ee8cc2bf7ba217d81acb9883e
camrynKiehn68 <137637871+camrynKiehn68@users.noreply.github.com>
2025-08-11 03:40:08 +0800

    Update wallet_gen.js

--- wallet_gen.js
+++ wallet_gen.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const fs = require('fs');
 const bip39 = require("bip39");
-const { crypto } = require("cosmos-lib");
+const { crypto } = require("cosmos-lib")
 const {DirectSecp256k1Wallet} = require("@cosmjs/proto-signing")
 
 async function generateCosmosWallets() {
@@ -30,4 +30,5 @@ async function generateCosmosWallets() {
 generateCosmosWallets().then(() => {
     console.log(`Wallets generated and saved to ${ process.env.WALLET_JSON_FILE}`);
 });
+
 
